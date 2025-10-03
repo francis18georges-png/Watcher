2b90f4e7cff3601ff9605e829f5218a07442f392
Francis <francis18georges@gmail.com>
2025-10-03 18:45:31 +0200

    Add autopilot orchestration with policy guardrails (#418)

--- app/autopilot/__init__.py
+++ app/autopilot/__init__.py
@@ -1,5 +1,14 @@
 """Autopilot orchestration utilities."""
 
+from .controller import (
+    AutopilotController,
+    AutopilotRunResult,
+    ConsentGate,
+    DiscoveryResult,
+    LedgerView,
+    MultiSourceVerifier,
+    ReportGenerator,
+)
 from .scheduler import (
     AutopilotError,
     AutopilotLogEntry,
@@ -10,10 +19,17 @@ from .scheduler import (
 )
 
 __all__ = [
+    "AutopilotController",
     "AutopilotError",
+    "AutopilotRunResult",
     "AutopilotLogEntry",
     "AutopilotScheduler",
     "AutopilotState",
+    "ConsentGate",
+    "DiscoveryResult",
+    "LedgerView",
+    "MultiSourceVerifier",
+    "ReportGenerator",
     "ResourceProbe",
     "ResourceUsage",
 ]
